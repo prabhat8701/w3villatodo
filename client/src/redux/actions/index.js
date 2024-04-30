b7a95df9fbe889618c6824cfc1eb730f63433f5e
Prabhat Singh <85447224+prabhat8701@users.noreply.github.com>
2024-04-30 21:15:17 +0530

    Update index.js

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -6,7 +6,7 @@ const API_URL = 'https://w3villatodo.vercel.app';
 
 export const addNewTodo = (data) => async (dispatch) => {
     try {
-        const res = await axios.post(`${API_URL}/todos`, { data });
+        const res = await axios.post(`${API_URL}todos`, { data });
 
         dispatch({ type: ADDNEW_TODO , payload: res.data });
     } catch (error) {
